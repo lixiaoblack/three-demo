a3760188e7bb0027190aee0a5cc37d166ef22557
xiaoyao <15029021831@163.com>
2025-04-02 18:41:45 +0800

    feat: 完成自动化部署

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+###
+ # @Author: wanglx
+ # @Date: 2025-04-02 18:37:19
+ # @LastEditors: wanglx
+ # @LastEditTime: 2025-04-02 18:39:54
+ # @Description: 自动部署脚本
+ # 
+ # Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
+### 
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 构建
+pnpm build
+
+# 进入构建输出目录
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 GitHub Pages
+git push -f git@github.com:lixiaoblack/three-demo.git main:gh-pages
+
+cd - 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: wanglx
+ * @Date: 2025-03-13 20:40:21
+ * @LastEditors: wanglx
+ * @LastEditTime: 2025-04-02 18:37:56
+ * @Description: Vite 配置文件
+ *
+ * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved.
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: "/three-demo/", // GitHub Pages 的仓库名
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
